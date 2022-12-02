e3957eabfd43d9978d495bb3c4dc1f70251b9ba9
Marina Varfolomeeva <marina.varfolomeeva@gmail.com>
2022-12-02 23:06:32 +0300

    before dist

--- 09_dist_code.R
+++ 09_dist_code.R
@@ -24,7 +24,9 @@ table(pos$Pop, pos$site)
 ## Неметрическое многомерное шкалирование ########
 
 library(vegan)
-ord_euclid <- metaMDS(pos[, 6:14], distance = "euclid", autotransform = FALSE)
+ord_euclid <- metaMDS(pos[, 6:14],
+                      distance = "euclid",
+                      autotransform = FALSE)
 
 
 ## Качество подгонки модели
@@ -39,10 +41,13 @@ ordiplot(ord_euclid, type = "t", cex = 0.5)
 
 ## Задание 1 -------------------------------------
 #
-# При помощи `ggplot2` постройте график неметрического многомерного шкалирования.
-# Для графика используйте координаты точек `ord_euclid$points` и исходные данные.
+# При помощи `ggplot2` постройте график
+# неметрического многомерного шкалирования.
+# Для графика используйте координаты точек
+# `ord_euclid$points` и исходные данные.
 # Раскрасьте график по значениям переменной `Pop`.
-# Сделайте так, чтобы особи разного пола были изображены на разных панелях
+# Сделайте так, чтобы особи разного пола были
+# изображены на разных панелях
 #
 # Дополните код
 
@@ -57,19 +62,23 @@ gg_euclid
 
 ## Задание 2 -------------------------------------
 #
-# Постройте nMDS ординацию при помощи евклидова расстояния, по стандартизованным данным
+# Постройте nMDS ординацию при помощи евклидова
+# расстояния, по стандартизованным данным
 
 # Дополните код
 
 # Ординация
-ord_scaled <- metaMDS( (pos), distance = , autotransform = )
+ord_scaled <- metaMDS( (pos),
+                       distance = ,
+                       autotransform = )
 # Качество ординации
 
 
 
 
 
-## График ординации по матрице евклидовых расстояний, рассчитанных по стандартизованным данным
+## График ординации по матрице евклидовых расстояний,
+# рассчитанных по стандартизованным данным
 # Данные для графика
 points_scaled <- data.frame(ord_scaled$points, pos)
 # График nMDS-ординации
@@ -77,7 +86,8 @@ gg_scaled <- gg_euclid %+% points_scaled
 gg_scaled
 
 
-## Видно, что графики ординации, полученные разными методами, различаются
+## Видно, что графики ординации, полученные
+# разными методами, различаются
 library(gridExtra)
 grid.arrange(gg_euclid + aes(size = age),
              gg_scaled + aes(size = age),
@@ -96,17 +106,20 @@ data(fossum)
 
 rownames(fossum) # Было
 
-# Чтобы имена строк были более информативны, добавим к ним название популяции
+# Чтобы имена строк были более информативны,
+# добавим к ним название популяции
 rownames(fossum) <- paste(fossum$Pop,
                           rownames(fossum),
                           sep = "_")
 rownames(fossum) # стало
 
-## Отбираем только то, что понадобится для кластеризации
+## Отбираем только то, что понадобится
+# для кластеризации
 fos <- fossum[complete.cases(fossum), 6:14]
 
 
-## Кластерный анализ начинается с расчета матрицы расстояний между объектами
+## Кластерный анализ начинается с расчета
+# матрицы расстояний между объектами
 
 d <- dist(x = fos, method = "euclidean")
 
@@ -118,28 +131,32 @@ hc_single <- hclust(d, method = "single")
 library(ape)
 ph_single <- as.phylo(hc_single)
 # cex - относительный размер шрифта
-plot(ph_single, type = "phylogram", direction = "downwards", cex = 0.7)
+plot(ph_single, type = "phylogram",
+     direction = "downwards", cex = 0.7)
 axisPhylo(side = 2)
 
 
 ## Метод отдаленного соседа ######################
 
 ph_compl <- as.phylo(hclust(d, method = "complete"))
-plot(ph_compl, type = "phylogram", direction = "downwards", cex = 0.8)
+plot(ph_compl, type = "phylogram",
+     direction = "downwards", cex = 0.8)
 axisPhylo(side = 2)
 
 
 ## Метод невзвешенного попарного среднего ########
 
 ph_avg <- as.phylo(hclust(d, method = "average"))
-plot(ph_avg, type = "phylogram", direction = "downwards", cex = 0.8)
+plot(ph_avg, type = "phylogram",
+     direction = "downwards", cex = 0.8)
 axisPhylo(side = 2)
 
 
 ## Метод Варда ###################################
 
 ph_w2 <- as.phylo(hclust(d, method = "ward.D2"))
-plot(ph_w2, type = "phylogram", direction = "downwards", cex = 0.8)
+plot(ph_w2, type = "phylogram",
+     direction = "downwards", cex = 0.8)
 axisPhylo(side = 2)
 
 
@@ -178,13 +195,18 @@ cl_boot <- pvclust(scale(t(fos)),
 plot(cl_boot, cex.pv = 0.8, cex = 0.8)
 
 
-### Построение деревьев по генетическим данным ####
+## Построение деревьев по генетическим данным ####
 
 ## Пример: Митохондриальная ДНК приматов. ########
 
-# Датасет собан Dr. Masami Hasegawa (Institute of Statistical Mathematics, Tokyo), по данным сиквенирования Kenji Hayasaka, Takashi Gojobori, Satoshi Horai (Molecular Biology and Evolution 5: 626-644, 1988).
-#
-# Исходный файл в формате PHYLIP можно загрузить по ссылке: http://evolution.genetics.washington.edu/book/primates.dna
+# Датасет собан Dr. Masami Hasegawa
+# (Institute of Statistical Mathematics, Tokyo),
+# по данным сиквенирования Kenji Hayasaka,
+# Takashi Gojobori, Satoshi Horai
+# (Molecular Biology and Evolution 5: 626-644, 1988).
+# Исходный файл в формате PHYLIP
+# можно загрузить по ссылке:
+# http://evolution.genetics.washington.edu/book/primates.dna
 
 webpage <-"http://evolution.genetics.washington.edu/book/primates.dna"
 primates.dna <- read.dna(webpage)
@@ -193,3 +215,8 @@ hc_pri <- hclust(d_pri, method = "average")
 ph_pri <- as.phylo(hc_pri)
 plot(ph_pri, cex = 0.8)
 axisPhylo()
+
+library(phangorn)
+# neighbor-joining (Saitou, Nei, 1987)
+ph_pri_NJ <- NJ(d_pri)
+plot(ph_pri_NJ, cex = 0.8)
